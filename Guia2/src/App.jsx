6afb7d347071b484232e5478f83e854d02dd9bb1
astridayala <astriayala06@gmail.com>
2025-01-24 14:21:06 -0600

    guia 2

---
+++ Guia2/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import { db } from './data/db'
+import { Guitar } from './components/Guitar'
+
+function App() {
+
+  const [data, setData] = useState(db)
+  console.log(data)
+
+  return (
+    <>
+    <Header/>
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          <Guitar/>
+          <Guitar/>
+        </div>
+    </main>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
