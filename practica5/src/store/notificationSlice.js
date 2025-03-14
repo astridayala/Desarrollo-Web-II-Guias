90c022c93a94b1944e5c8cd96627f1758e3b8acc
astridayala <astriayala06@gmail.com>
2025-03-14 11:09:26 -0600

    commit

--- practica5/src/store/notificationSlice.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-export const useNotificationStore = create((set) => ({
-    notification: null,
-    showNotification: (message, type = 'sucess') => set({ notification: {message, type} }),
-    hideNotification: () => set({notification:null}),
-}))
